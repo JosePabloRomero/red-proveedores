65ba2f72ef2fe1f81d5a988288231a290099b032
Jose Pablo <josepablo.developer@gmail.com>
2020-10-10 13:15:25 -0500

    Se implementa primera parte del Formulario Registro de Usuarios

---
+++ pages/registro-usuarios.vue
@@ -0,0 +1,139 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col class="primary" align="center">
+        <h1>Formulario Registro de Usuarios</h1>
+      </v-col>
+    </v-row>
+
+    <v-form v-model="formUsers" ref="formUsers">
+      <!-- Seleccionar un rol -->
+      <v-row justify="center">
+        <v-col cols="12" sm="4" class="mt-5">
+          <v-select
+            :items="rol"
+            @change="actualizarRol"
+            v-model="rolSeleccionado"
+            label="Seleccione un rol"
+            outlined
+          ></v-select>
+        </v-col>
+      </v-row>
+
+      <!-- Formularios generales para proveedor y usuario -->
+      <v-row v-if="rolSeleccionado">
+        <v-col sm="4" v-for="(item, index) in camposGenerales" :key="index">
+          <v-text-field
+            :label="item.label"
+            v-model="item.dato"
+            :rules="fieldRequired"
+            :type="item.type"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <!-- Campos solo para proveedores -->
+      <v-row v-if="rolSeleccionado === rol[0]">
+        <v-col md="6">
+          <v-select
+            :label="tipoId.label"
+            :items="tipoId.items"
+            :v-model="tipoId.tipoSeleccionado"
+            required
+          ></v-select>
+        </v-col>
+
+        <v-col md="6" v-for="(item, index) in camposProveedor" :key="index">
+          <v-text-field
+            :label="item.label"
+            v-model="item.dato"
+            required
+            :rules="fieldRequired"
+          >
+          </v-text-field>
+        </v-col>
+
+        <v-col md="6">
+          <v-textarea
+            label="Descripción"
+            filled
+            auto-grow
+            rows="2"
+            row-height="30"
+            :rules="fieldRequired"
+            required
+            v-model="descripcion"
+          >
+          </v-textarea>
+        </v-col>
+      </v-row>
+      <v-row justify="center" v-if="rolSeleccionado">
+        <v-col md="4">
+          <v-btn color="success" block>Enviar </v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formUsers: null,
+      rol: ["Proveedor", "Usuario"],
+      rolSeleccionado: null,
+      camposGenerales: [],
+      camposProveedor: [
+        { label: "Numero Identificación", dato: "" },
+        { label: "Dirección del proveedor", dato: "" },
+      ],
+      descripcion: null,
+      tipoId: {
+        label: "Tipo de Identificación",
+        items: ["cedula", "pasaporte"],
+        tipoSeleccionado: null,
+      },
+      /* Comprobar Campos */
+      fieldRequired: [(v) => !!v || "Este campo es requerido"],
+      emailRules: [
+        (v) => !!v || "El e-mail es requerido",
+        (v) => /.+@.+/.test(v) || "El e-mail debe de ser valido",
+      ],
+    };
+  },
+  methods: {
+    actualizarRol() {
+      this.camposGenerales = [
+        {
+          label: `Nombre del ${this.rolSeleccionado}`,
+          dato: "",
+          type: "text",
+        },
+        {
+          label: `Apellido del ${this.rolSeleccionado}`,
+          dato: "",
+          type: "text",
+        },
+        {
+          label: `E-mail del ${this.rolSeleccionado}`,
+          dato: "",
+          type: "email",
+        },
+        {
+          label: `Contraseña del ${this.rolSeleccionado}`,
+          dato: "",
+          type: "password",
+        },
+        /* Es necesario agregar este campo? */
+        {
+          label: `Telefono del ${this.rolSeleccionado}`,
+          dato: "",
+          type: "text",
+        },
+      ];
+    },
+  },
+};
+</script>
