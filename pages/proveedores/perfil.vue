96e4b4fed02e84293f1c4dcd44515aecb336c356
Paulina Velásquez <mapaulina09@hotmail.com>
2020-11-15 00:20:29 -0500

    Conexión de backend con categorías de proveedor

--- pages/proveedores/perfil.vue
+++ pages/proveedores/perfil.vue
@@ -22,11 +22,11 @@
                 ></v-carousel-item>
               </v-carousel>
             </v-img>
-            <v-card-tittle>
+            <v-card-title>
                 <center class="mt-6">
-                    <v-chip v-for="(item, index) in categorias" :key="index" class="mr-2">{{item.nombre}}</v-chip>
+                    <v-chip v-for="(item, index) in categorias" :key="index" class="mr-2">{{item.nombre_de_la_categoria}}</v-chip>
                 </center>               
-            </v-card-tittle>
+            </v-card-title>
             <v-card-text>
               <h3> Descripción </h3>
               <v-card-subtitle> {{descripcion}} </v-card-subtitle>
@@ -78,13 +78,14 @@ export default {
           src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
         },
       ],
+      id: "",
       nombre: "",
       apellido: "",
       descripcion: "",
       contacto: "",
       direccion: "",
       show: false,
-      categorias: [{nombre:"Insumos agrícolas"}, {nombre:"Lácteos"}]
+      categorias: []
     };
   },
   methods: {
@@ -95,16 +96,16 @@ export default {
       let token = localStorage.getItem("token");
       this.$axios.setToken(token, "Bearer");
       let { data } = await this.$axios.get(url + "auth");
+      this.id = data.info.id;
       this.nombre = data.info.nombre;
       this.apellido = data.info.apellido;
       this.descripcion = data.info.descripcion;
       this.direccion = data.info.direccion;
       this.contacto = data.info.contacto;
+      let categorias = await this.$axios.get(url + "categorias_proveedor/" + "4")
+      this.categorias = categorias.data.info;
+      console.log(this.categorias)
     },
-    async cargarCateogorias() {
-        let { data } = await this.$axios.get(url + "categorias")
-        this.ventas = data.info;
-    }
   },
 };
 </script>
